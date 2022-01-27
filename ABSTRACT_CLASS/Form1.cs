38040c64ac45961a036ae85ef1bf7fe8e4db7e09
zouhair <ettarak.zouhair@gmail.com>
2022-01-27 22:05:08 +0100

    Abstract Class ovverid polymorphism

---
+++ ABSTRACT_CLASS/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ABSTRACT_CLASS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        abstract class vehicule
+        {
+            public abstract double getFieulAmount();
+        }
+
+        class Bus: vehicule
+        {
+            public override double getFieulAmount()
+            {
+                return 240;
+            }
+        }
+
+        class Track: vehicule
+        {
+            public override double getFieulAmount()
+            {
+                return 1400;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Text = "";
+            vehicule vc;
+            Bus bus = new Bus();
+            Track track = new Track();
+            Text += "Abstract Calss Bus : "+  bus.getFieulAmount() + Environment.NewLine;
+            Text += "Abstract Class Track: "+ track.getFieulAmount() + Environment.NewLine;
+
+            textBox1.Text = Text;
+
+        }
+    }
+}
